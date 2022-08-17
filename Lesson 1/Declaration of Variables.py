813be5d0596166b5e38d1827a9c54d3b041b9537
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-08-18 10:05:41 +1200

    Add files via upload

---
+++ Lesson 1/Declaration of Variables.py
@@ -0,0 +1,12 @@
+# Declaration of Variables
+
+base = 5.4
+height = 3
+
+# The Statement to Calculate the Area (Using Variables)
+
+(base*height)/2
+
+# Print the Result to screen
+
+Print(base*heigt)/2
