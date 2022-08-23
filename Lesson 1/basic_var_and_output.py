240ec8558a2f79d1c93a24a14ca235c7bb37ff14
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-08-23 14:49:21 +1200

    Add files via upload

---
+++ Lesson 1/basic_var_and_output.py
@@ -0,0 +1,14 @@
+# S M Chow Worn
+# Variables
+
+# Declaration of Variables
+Name = 'Lion'
+munLegs = 4
+hasFur = True
+height = 1.2
+
+# Output to Screen
+print(name)
+print(munLegs)
+print(hasFur)
+print(height)
---
+++ Lesson 1/basic_var_output_show_datatype.py
@@ -0,0 +1,14 @@
+# S M Chow Worn
+# Variables, data types
+
+# Declaration of Variables
+Name = 'Lion'
+munLegs = 4
+hasFur = True
+height = 1.2
+
+# Output data type to Screen
+print(type(name))
+print(type(munLegs))
+print(type(hasFur))
+print(type(height))
