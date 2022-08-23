784fbc80aef746077783ca114c24e9fc5540d732
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-08-23 15:02:01 +1200

    Add files via upload

---
+++ Lesson 1/basic_var_output_with_labels.py
@@ -0,0 +1,14 @@
+# S M Chow Worn
+# Variables
+
+# Declaration of Variables
+name = 'Lion'
+munLegs = 4
+hasFur = True
+height = 1.2
+
+# Output to Screen
+print("One of the big five animals in Africa is called a" ,name, ".")
+print("A" ,name, "has" ,numLegs, "legs.")
+print("Yes, it's" ,hasFur, "it has fur")
+print("And it's height is approximately" ,height, "metres.")
