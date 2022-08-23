c6ef48742e1ebc77dd08308d21118583d56284c0
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-08-23 15:04:03 +1200

    Update basic_var_output_with_labels.py

--- Lesson 1/basic_var_output_with_labels.py
+++ Lesson 1/basic_var_output_with_labels.py
@@ -10,5 +10,5 @@ height = 1.2
 # Output to Screen
 print("One of the big five animals in Africa is called a" ,name, ".")
 print("A" ,name, "has" ,numLegs, "legs.")
-print("Yes, it's" ,hasFur, "it has fur")
+print("Yes, it's" ,hasFur, ". It has fur")
 print("And it's height is approximately" ,height, "metres.")
