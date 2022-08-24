184a7e3cd9e6722d751d349b0b969778e2a259a9
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-08-25 09:45:43 +1200

    Update basic_var_and_output.py

--- Lesson 1/basic_var_and_output.py
+++ Lesson 1/basic_var_and_output.py
@@ -3,12 +3,12 @@
 
 # Declaration of Variables
 name = 'Lion'
-munLegs = 4
+numLegs = 4
 hasFur = True
 height = 1.2
 
 # Output to Screen
 print(name)
-print(munLegs)
+print(numLegs)
 print(hasFur)
 print(height)
