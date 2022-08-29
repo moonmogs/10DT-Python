c7f1d08c539760f197b2d77d7ad8939cf169a8ce
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-08-29 13:36:43 +1200

    Add files via upload

---
+++ Lesson 1/L1_practice_task.py
@@ -0,0 +1,24 @@
+# S M Chow Worn
+# Focus - Variables, Labels, Comments, Data Types
+
+# Declaration of Variables
+name = 'S M Chow Worn'
+height = "180cm"
+adress = "170 Couttes Street"
+mobileNumb = "0272121051"
+homeNumb = "3862180"
+
+# Display Variables on Screen
+print(name)
+print(height)
+print(adress)
+print(mobileNumb)
+print(homeNumb)
+
+# Output Data with Context
+
+print("My name is "(name)".")
+print("I am "(height)"tall.")
+print("My adress is "(adress)".")
+print("My mobile phone number is "(mobileNumb)".")
+print("And my home number is "(homeNumb)".")
