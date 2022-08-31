757f303599ba338c44a585e7701dbdf5445f8f8e
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-09-01 10:02:15 +1200

    Add files via upload

---
+++ Lesson 1/input_from_user.py
@@ -0,0 +1,48 @@
+# S M Chow Worn
+# User input
+
+#name = input("Enter your first name: ")
+
+#print("Hello,", name, ", I hope you have a good day.")
+
+# data types for input
+# heights
+# favourite colour
+# number of pets
+# how do you come to school
+
+#height = input("How tall are you?: ")
+
+#print("So you are", height, "tall then?")
+
+#colour = input("Enter your favourite colour: ")
+
+#print("You like", colour, "then? Nice.")
+
+#numbPets = input("How many pets do you have?: ")
+
+#print("You have", numbPets, "pets.")
+
+#commute = input("How do you come to work/shool?: ")
+
+#print("So you normally", commute, "to get to work/school?")
+
+# taking input as an interger
+#num = int(input("Enter a number:"))
+#add = num + 1
+
+# Output
+#print(add)
+#taking input as a float
+#num = float(input("Enter a number:"))
+#add = num + 1
+
+# Output
+#print(add)
+# taking input as a list
+#li = list(input("Enter a number of 5 digits: "))
+#print(li)
+
+# taking input as a tuple
+num = tuple(input("Enter a number: "))
+print(tuple)
