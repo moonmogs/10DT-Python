2aa31ebe718ed1d238fbd362f2f235e7e427c435
Sebastien CW <103608409+moonmogs@users.noreply.github.com>
2022-09-12 13:14:07 +1200

    Add files via upload

---
+++ Lesson 2/user_input_calc.py
@@ -0,0 +1,63 @@
+# S M Chow Worn
+# User input types
+
+# Taking input from the user
+# string = input()
+# Output
+# print(string)
+
+# taking input from the user
+# name = input("Enter your name")
+# Output
+# print(name)
+
+# taking input from the user
+# name = input("Enter your name: ")
+# Output
+# print(name)
+
+# Taking input from the user as integer
+# num = int(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from the user as float
+# num = float(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from the user as list
+# li = list(input("Enter a number: "))
+# Output
+# print(li)
+
+# Basic input example
+num = input("Enter a number: ")
+num2 = input("Enter another number: ")
+# Output as add, sub, multiply, divide
+
+# Add
+# Basic input example
+num = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print("The sum is " + str(int(num) + int(num2)))
+
+# Subtract
+# Basic input example
+num = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print("The sum is " + str(int(num) - int(num2)))
+
+# Multiply
+# Basic input example
+num = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print("The sum is " + str(int(num) * int(num2)))
+
+# Basic input example
+num = input("Enter a number: ")
+num2 = input("Enter another number: ")
+# Divide
+print("The sum is " + str(int(num) / int(num2)))
